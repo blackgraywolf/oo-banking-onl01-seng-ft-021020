481483c78c6dc9e2b0004ef10201f7fae1ffbc3a
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-27 21:50:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
 
 
   def execute_transaction
-    if @sender.balance <= @amount 
+    if @sender.balance <= @amount && valid?
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
